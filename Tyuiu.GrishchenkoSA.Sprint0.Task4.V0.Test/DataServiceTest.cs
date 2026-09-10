a12f5dd66733918700e4bf8fed676528f9a5e14c
GrishchenkoSemyon <rashasemen@icloud.com>
2026-09-10 11:38:51 +0300

    Task4

---
+++ Tyuiu.GrishchenkoSA.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tyuiu.GrishchenkoSA.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
